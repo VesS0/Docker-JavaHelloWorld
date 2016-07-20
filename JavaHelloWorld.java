e259186bab15ab9bc8bbda67f10a3c2bedf6e5f0
vess0 <vvesic@yahoo.com>
2016-07-20 14:48:52 +0530

    Oooo bozeee ;;;;;

--- JavaHelloWorld.java
+++ JavaHelloWorld.java
@@ -2,6 +2,6 @@ public class JavaHelloWorld{
 	public static void main(String [] args)
 	{
 		System.out.println("Cao svete poz");
-		System.out.println("Cao konzoloo")
+		System.out.println("Cao konzoloo");
 	}
 }
